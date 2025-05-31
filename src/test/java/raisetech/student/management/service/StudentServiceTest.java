215cae646281eb59ec846af4dfadaccf5b818441
Ringo450 <appointment3310@gmail.com>
2025-05-31 20:23:26 +0900

    第27回-Testクラスに①@ExtendWith(MockitoExtension.class)　②@Mock private StudentRepository repository;　③@Mock private StudentConverter converter;　を追加し、Mock化した。

--- src/test/java/raisetech/student/management/service/StudentServiceTest.java
+++ src/test/java/raisetech/student/management/service/StudentServiceTest.java
@@ -4,9 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import raisetech.student.management.controller.converter.StudentConverter;
 import raisetech.student.management.domain.StudentDetail;
+import raisetech.student.management.repository.StudentRepository;
 
+@ExtendWith(MockitoExtension.class)
 class StudentServiceTest {
+
+  @Mock
+  private StudentRepository repository;
+
+  @Mock
+  private StudentConverter converter;
 
   @Test
   void 受講生詳細一覧検索＿リポジトリとコンバーターの処理が適切に呼び出せていること() {//メソッドや変数名には、日本語使える！
